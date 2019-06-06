698ef3e796fb32491bbd73effcbb1953644f8fac
Lazareva Vlada <vladalazareva@mail.ru>
2019-06-06 21:07:33 +0300

    change css, html

--- src/app/components/input-switch/input-switch.component.ts
+++ src/app/components/input-switch/input-switch.component.ts
@@ -1,24 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import {Component,  OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-input-switch',
   templateUrl: './input-switch.component.html',
   styleUrls: ['./input-switch.component.scss']
 })
-export class InputSwitchComponent implements OnInit {
 
-  checked: boolean;
+export class InputSwitchComponent implements OnInit {
 
   constructor() { }
 
   ngOnInit() {
   }
 
-  onClick() {
-    if (this.checked === false) {
-      this.checked = true;
-    } else {
-      this.checked = false;
-    }
-  }
 }
