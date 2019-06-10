ea4100b90be62cfa09b3e41ab0340baa55cd4814
Lazareva Vlada <vladalazareva@mail.ru>
2019-06-10 16:53:36 +0300

    do something

--- src/app/components/input-switch/input-switch.component.ts
+++ src/app/components/input-switch/input-switch.component.ts
@@ -1,4 +1,4 @@
-import {Component,  OnInit} from '@angular/core';
+import {ChangeDetectorRef, Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 
 @Component({
   selector: 'app-input-switch',
@@ -8,7 +8,41 @@ import {Component,  OnInit} from '@angular/core';
 
 export class InputSwitchComponent implements OnInit {
 
-  constructor() { }
+  @Input() readonly: boolean;
+  @Output() onChange: EventEmitter<any> = new EventEmitter();
+
+  checked = false;
+
+  onModelChange: Function = () => {};
+
+  constructor() {}
+
+  onClick(event: Event, cb: HTMLInputElement) {
+    if (!this.readonly) {
+      this.toggle(event);
+      cb.focus();
+    }
+  }
+
+  onInputChange(event: Event) {
+    if (!this.readonly) {
+      const inputChecked = (<HTMLInputElement> event.target).checked;
+      this.updateModel(event, inputChecked);
+    }
+  }
+
+  toggle(event: Event) {
+    this.updateModel(event, !this.checked);
+  }
+
+  updateModel(event: Event, value: boolean) {
+    this.checked = value;
+    this.onModelChange(this.checked);
+    this.onChange.emit({
+      originalEvent: event,
+      checked: this.checked
+    });
+  }
 
   ngOnInit() {
   }
