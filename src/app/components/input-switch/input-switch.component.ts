54d208c9559625e793628443c66b1a1604ab9dfa
Lazareva Vlada <vladalazareva@mail.ru>
2019-06-10 17:32:53 +0300

    add only onClick

--- src/app/components/input-switch/input-switch.component.ts
+++ src/app/components/input-switch/input-switch.component.ts
@@ -11,36 +11,15 @@ export class InputSwitchComponent implements OnInit {
   @Input() readonly: boolean;
   @Output() onChange: EventEmitter<any> = new EventEmitter();
 
-  checked = false;
-
-  onModelChange: Function = () => {};
+  checked1 = false;
 
   constructor() {}
 
-  onClick(event: Event, cb: HTMLInputElement) {
-    if (!this.readonly) {
-      this.toggle(event);
-      cb.focus();
-    }
-  }
-
-  onInputChange(event: Event) {
-    if (!this.readonly) {
-      const inputChecked = (<HTMLInputElement> event.target).checked;
-      this.updateModel(event, inputChecked);
-    }
-  }
-
-  toggle(event: Event) {
-    this.updateModel(event, !this.checked);
-  }
-
-  updateModel(event: Event, value: boolean) {
-    this.checked = value;
-    this.onModelChange(this.checked);
+  onClick(event: Event) {
+    this.checked1 = (event.target as HTMLInputElement).checked;
     this.onChange.emit({
       originalEvent: event,
-      checked: this.checked
+      checked: this.checked1
     });
   }
 
