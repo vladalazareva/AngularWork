f5d638fa816993397c9d936b6100392959da6efa
Lazareva Vlada <vladalazareva@mail.ru>
2019-07-04 15:21:20 +0300

    change Tooltip component, add tooletip-showcase

--- src/app/components/tooltip/tooltip.component.ts
+++ src/app/components/tooltip/tooltip.component.ts
@@ -1,15 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input} from '@angular/core';
 
 @Component({
   selector: 'app-tooltip',
   templateUrl: './tooltip.component.html',
   styleUrls: ['./tooltip.component.scss']
 })
-export class TooltipComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
+export class TooltipComponent {
+  @Input() left: boolean;
+  @Input() right: boolean;
+  @Input() top: boolean;
+  @Input() buttom: boolean;
 
 }
