67d10505c5d516402e268bb9ae76a92364df2129
Lazareva Vlada <vladalazareva@mail.ru>
2019-07-11 12:14:43 +0300

    add title, 4 tooltip with position, add style

--- src/app/directives/tooltip.directive.ts
+++ src/app/directives/tooltip.directive.ts
@@ -22,17 +22,51 @@ export class TooltipDirective {
   }
 
   private createTooltip() {
-    let position = this.el.nativeElement.getBoundingClientRect();
-    const topPos = position.top + window.scrollY;
-    const leftPos = position.left + window.scrollX + position.width/2 ;
-
     let div = document.createElement('div');
-    let modifier = this.tooltipPosition || 'left';
+    let modifier = this.tooltipPosition || 'bottom';
+
+    let position = this.el.nativeElement.getBoundingClientRect();
+    let topPos = position.top + window.scrollY - 10 ;
+    let leftPos = position.left + window.scrollX + position.width/2 ;
+
+
     div.className = 'tooltip tooltip_' + modifier;
-    div.innerHTML = `<div class = "tooltip__arrow"></div> 
+
+    switch(modifier){
+      case 'right':
+        leftPos = leftPos + 125;
+        div.style.left = `${leftPos}px`;
+        div.style.top = `${topPos}px`;
+        div.innerHTML = `<div class = "tooltip__arrowRight" ></div> 
                      <div class = "tooltip__text" >${this.tooltipText}</div>`;
-    div.style.left = `${leftPos}px`;
-    div.style.top = `${topPos}px`;
+        break;
+      case 'left':
+        leftPos = leftPos - 120;
+        div.style.left = `${leftPos}px`;
+        div.style.top = `${topPos}px`;
+        div.innerHTML = ` <div class = "tooltip__text" >${this.tooltipText}</div>
+                          <div class = "tooltip__arrowLeft" ></div> 
+                    `;
+        break;
+      case 'top':
+        topPos = topPos - 30;
+        div.style.top = `${topPos}px`;
+        div.style.left = `${leftPos}px`;
+        div.innerHTML = ` <div class = "tooltip__text" >${this.tooltipText}</div>
+                          <div class = "tooltip__arrowTop"></div> 
+                    `;
+        break;
+      case 'bottom':
+        topPos = topPos + 30;
+        div.style.top = `${topPos}px`;
+        div.style.left = `${leftPos}px`;
+        div.style.flex = "column";
+        div.innerHTML = `<div class = "tooltip__arrowBottom"></div> 
+                     <div class = "tooltip__text" >${this.tooltipText}</div>`;
+        break;
+    }
+    //div.style.left = `${leftPos}px`;
+    //div.style.top = `${topPos}px`;
     this.element = div;
 
   }
